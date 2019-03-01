dcdbf06f5b8884ea69c32869c5958ada1131c6ac
ishara11rathnayake <ishara11rathnayake@gmail.com>
2019-03-02 00:30:45 +0530

    finished sending messages option

--- app/src/main/java/com/industrialmaster/trustme/SendSMSReceiver.java
+++ app/src/main/java/com/industrialmaster/trustme/SendSMSReceiver.java
@@ -1,21 +1,38 @@
 package com.industrialmaster.trustme;
 
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.telephony.SmsManager;
 import android.telephony.TelephonyManager;
-import android.widget.Toast;
+
+import static android.content.Context.MODE_PRIVATE;
 
 public class SendSMSReceiver extends BroadcastReceiver {
+
     @Override
-    public void onReceive(Context context, Intent intent) {
+    public void onReceive(final Context context, Intent intent) {
         Bundle extras = intent.getExtras();
         if(extras != null){
             String state = extras.getString(TelephonyManager.EXTRA_STATE);
             if(state.equals(TelephonyManager.EXTRA_STATE_RINGING)){
                 String phoneNo = extras.getString(TelephonyManager.EXTRA_INCOMING_NUMBER);
-                Toast.makeText(context, "You got a call from"+phoneNo, Toast.LENGTH_SHORT).show();
+
+                String receiverNo = null;
+
+                SharedPreferences prefs = context.getSharedPreferences("MyPrefsFile", MODE_PRIVATE);
+                receiverNo = prefs.getString("RecieverNo", "0769600614");
+
+                String smsMessage = "Your girlfriend got a call from " + phoneNo;
+
+                String scAddress = null;
+                PendingIntent sentIntent = null, deliveryIntent = null;
+
+                SmsManager smsManager = SmsManager.getDefault();
+                smsManager.sendTextMessage(receiverNo, scAddress, smsMessage, sentIntent, deliveryIntent);
             }
         }
     }
--- app/src/main/java/com/industrialmaster/trustme/SubscribeActivity.java
+++ app/src/main/java/com/industrialmaster/trustme/SubscribeActivity.java
@@ -1,13 +1,17 @@
 package com.industrialmaster.trustme;
 
 import android.content.IntentFilter;
+import android.content.SharedPreferences;
 import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class SubscribeActivity extends AppCompatActivity {
+
+    public static final String MY_PREFS_NAME = "MyPrefsFile";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,8 +20,17 @@ public class SubscribeActivity extends AppCompatActivity {
     }
 
     public void subscribe(View view){
+
+        EditText editTextMobile = (EditText) findViewById(R.id.editTextMobile);
+        String recieverNo = editTextMobile.getText().toString();
+
+        SharedPreferences.Editor editor = getSharedPreferences(MY_PREFS_NAME, MODE_PRIVATE).edit();
+        editor.putString("RecieverNo", recieverNo);
+        editor.apply();
+
         IntentFilter filter = new IntentFilter();
         filter.addAction("android.intent.action.PHONE_STATE");
+
         SendSMSReceiver receiver = new SendSMSReceiver();
 
         registerReceiver(receiver,filter);
@@ -30,4 +43,5 @@ public class SubscribeActivity extends AppCompatActivity {
         LocalBroadcastManager.getInstance(this).unregisterReceiver(receiver);
         Toast.makeText(this, "Unsubscribed", Toast.LENGTH_SHORT).show();
     }
+
 }
